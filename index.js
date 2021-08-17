ac6ad85cfdd99e5bd3aa4c6cb36eb76b4debddf9
lpoghi <85479967+lpoghi@users.noreply.github.com>
2021-08-17 15:35:27 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let age = 14;
+
+if (age == 18) {
+    log.console('Come in!');
+} else if (age > 18) {
+    log.console('Come in!');
+} else (age < 18)
+{
+    log.console('Do not enter!');
+}
+
+let isFemale = true;
+
+if (isFemale) {
+
+    console.log("woop woop come in to the Female party!");
+} else {
+    console.log(
+        "go away.."
+    );
+}
+
+let driverStatus = bob
+
+if (driverStatus == "bob") {
+    console.log("de bob mag rijden");
+} else {
+    console.log("Je bent geen bob, hier met die sleutels");
+}
+}
